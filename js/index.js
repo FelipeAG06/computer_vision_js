6328c647b30e2f28742c441fb864a1576c5c9b8c
Felipe Alvarez <felipeag06@gmail.com>
2017-09-24 23:56:03 -0500

    jsfeat02_gray_scale: Second example added

--- js/index.js
+++ js/index.js
@@ -1,12 +1,27 @@
 (function() {
-    var matrix = new jsfeat.matrix_t(2, 2, jsfeat.U8_t | jsfeat.C1_t);
-    
-    matrix.data[1] = 1;
-    matrix.data[5] = 3;
+    var canvas = document.getElementById('initCanvas'),
+	context = canvas.getContext('2d'),
+	image = new Image();
 
-    for(var i = 0; i < matrix.rows; i++) {
-        var start =  i * matrix.cols;
-        var end = start + matrix.cols;
-        console.log(matrix.data.subarray(start, end));
+	image.src = '../img/sw01.jpg';
+
+	image.onload = function() {
+		var cols = image.width;
+        var rows = image.height;
+        canvas.width = cols;
+        canvas.height = rows;
+        context.drawImage(image, 0, 0, image.width, image.height, 0, 0, cols, rows);
+
+        var canvasParent = document.getElementById('images');
+        var imageData = context.getImageData(0, 0, cols, rows);
+        
+        
+        var dataBuffer = new jsfeat.data_t(cols * rows, imageData.data.buffer);
+        var matrix = new jsfeat.matrix_t(cols, rows, jsfeat.U8_t | jsfeat.C4_t, dataBuffer);
+
+        var grayImage = new jsfeat.matrix_t(matrix.cols, matrix.rows, jsfeat.U8_t | jsfeat.C1_t);
+        jsfeat.imgproc.grayscale(matrix.data, matrix.cols, matrix.rows, grayImage);
+
+        drawMat(grayImage, canvasParent, 'canvas-img');
     }
 })();
