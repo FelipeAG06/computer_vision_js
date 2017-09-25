2bbdaa3f182432a61ac35ba4c1a018e49c8d7ada
Felipe Alvarez <felipeag06@gmail.com>
2017-09-25 15:54:21 -0500

    tracking05_tag_people: Initial changes

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,29 +1,46 @@
 'use strict';
 
 var canvas = document.getElementById('initCanvas');
-var context = canvas.getContext('2d');
+var imageWrapper = document.getElementById('imageWrapper');
+
+var theFoos = ['Pat Smear', 'Nate Mendel', 'Dave Grohl', 'Taylor Hawkins', 'Chris Shiftlett'];
 
 var trackFace = function trackFace() {
     var tracker = new tracking.ObjectTracker('face');
 
-    tracker.setInitialScale(4);
-    tracker.setStepSize(2);
-    tracker.setEdgesDensity(0.1);
-
     tracker.on('track', function (event) {
-        context.clearRect(0, 0, canvas.width, canvas.height);
+        var data = event.data;
+        data.sort(function (a, b) {
+            return b.x - a.x;
+        });
+
+        data = data.filter(function (element) {
+            return element.width >= 50;
+        });
+
         _.each(event.data, function (rect) {
             drawRect(rect.x, rect.y, rect.width, rect.height);
         });
     });
 
     var drawRect = function drawRect(x, y, w, h) {
-        context.lineWidth = '4';
-        context.strokeStyle = 'red';
-        context.strokeRect(x, y, w, h);
+        var rect = document.createElement('div');
+        var name = document.createElement('input');
+
+        name.value = theFoos.pop();
+        rect.appendChild(name);
+
+        imageWrapper.appendChild(rect);
+
+        rect.style.width = w + 'px';
+        rect.style.height = h + 'px';
+        rect.style.left = image.offsetLeft + x + 'px';
+        rect.style.top = image.offsetTop + y + 'px';
+        rect.style.position = 'absolute';
+        rect.style.border = '3px solid white';
     };
 
-    tracking.track('#video', tracker, { camera: true });
+    tracking.track('#image', tracker);
 };
 
 window.onload = function () {
--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,45 @@
 let canvas = document.getElementById('initCanvas');
-let context = canvas.getContext('2d');
+let imageWrapper = document.getElementById('imageWrapper');
+
+const theFoos = ['Pat Smear', 'Nate Mendel', 'Dave Grohl', 'Taylor Hawkins',
+'Chris Shiftlett']
 
 const trackFace = () => {
     const tracker = new tracking.ObjectTracker('face');
-    
-    tracker.setInitialScale(4);
-    tracker.setStepSize(2);
-    tracker.setEdgesDensity(0.1);
 
     tracker.on('track', (event) => {
-        context.clearRect(0, 0, canvas.width, canvas.height);
+        let data = event.data;
+        data.sort((a, b) => {
+            return b.x - a.x;
+        });
+
+        data = data.filter((element) => {
+            return element.width >= 50;
+        });
+
         _.each(event.data, (rect) => {
             drawRect(rect.x, rect.y, rect.width, rect.height);
         });
     });
 
     const drawRect = (x, y, w, h) => {
-        context.lineWidth = '4';
-        context.strokeStyle = 'red';
-        context.strokeRect(x, y, w, h);
+        let rect = document.createElement('div');
+        let name = document.createElement('input');
+
+        name.value = theFoos.pop();
+        rect.appendChild(name);
+
+        imageWrapper.appendChild(rect);
+
+        rect.style.width = w + 'px';
+        rect.style.height = h + 'px';
+        rect.style.left = (image.offsetLeft + x) + 'px';
+        rect.style.top = (image.offsetTop + y) + 'px';
+        rect.style.position = 'absolute';
+        rect.style.border = '3px solid white';
     }
 
-    tracking.track('#video', tracker, {camera: true});
+    tracking.track('#image', tracker);
 }
 
 window.onload = () => trackFace();
